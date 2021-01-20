95e39dd62822fb601d96c4afe3a3325b5b5f6ecd
MHJworld <50094131+MHJworld@users.noreply.github.com>
2021-01-20 15:33:17 +0900

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,37 +1,86 @@
-from flask import Flask, request, jsonify
+import tensorflow_hub as hub
+from flask import Flask
+from flask import request
+from flask import send_file
+from flask import jsonify
+import tensorflow as tf
 from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import numpy as np
 import io
+
+##########모델 로드
+
+base_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+
+placeholder = tf.keras.layers.Input(shape=(None, None, 3), dtype=tf.float32)
+placeholder_1 = tf.keras.layers.Input(shape=(None, None, 3), dtype=tf.float32)
+net = hub.KerasLayer(base_model, signature='serving_default', signature_outputs_as_dict=True)({'placeholder': placeholder, 'placeholder_1': placeholder_1})
+model = tf.keras.models.Model({'placeholder': placeholder, 'placeholder_1': placeholder_1}, net)
+
+##########모델 예측
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "hello world"
+def index():
+    html = '''
+<html>
+<head>
+    <title>스타일 변환</title>
+</head>
+<body>
+    <center>
+    스타일 변환<br>
+    <form action="/predict" method="post" enctype="multipart/form-data">
+        이미지 파일 <input type="file" name="file"><br>
+        <input type="submit" value="변환">
+    </form>
+    </center>
+</body>
+</html>
+'''
+
+    return html
 
 @app.route('/send_image', methods=['POST', 'GET'])
 def send_image():
     if request.method == "POST":
         if request.files.get("myImage") and request.files.get("styleImage") :
-            myImage = request.files["myImage"].read()
-            myImage = Image.open(io.BytesIO(myImage)).convert('RGB')
-            styleImage = request.files["styleImage"].read()
-            styleImage = Image.open(io.BytesIO(styleImage)).convert('RGB')
+            image = request.files["myImage"].read()
+            image = Image.open(io.BytesIO(image)).convert('RGB')
+            # image.show()
+            width, height = image.size
+            image_numpy = np.array(image)
+            x_test = np.array([image_numpy])
+            x_test = x_test / 255
+            content_image = x_test
 
-            # [DEBUG] show image
-            print("here ok~")
-            myImage.show()
-            styleImage.show()
+            image = request.files["styleImage"].read()
+            image = Image.open(io.BytesIO(image)).convert('RGB')
+            # image.show()
+            image = image.resize((width, height))
+            image_numpy = np.array(image)
+            x_test = np.array([image_numpy])
+            x_test = x_test / 255
+            style_image = x_test
 
-            # 모델 실행
-            # import styleTransfer as sf
-            # result = ""
-            # sf.predict(myImageFile, styleImageFile)
-            # result = sf.main(myImage, styleImage)
-            return jsonify({"result": "get image ok!!!!"})
+            dict_outut = model.predict({'placeholder': content_image, 'placeholder_1': style_image})
+
+            y_predict = dict_outut['output_0']
+            numpy_image = y_predict[0]
+            numpy_image = (numpy_image * 255).astype(np.uint8)
+            image = Image.fromarray(numpy_image)
+            bytesIO = io.BytesIO()
+            image.save(bytesIO, 'JPEG', quality=70)
+            bytesIO.seek(0)
+            image.show()
+            
+            return send_file(bytesIO, mimetype='image/jpeg')
         else:
             return jsonify({"result": "failed to get image"})
     else:
         return "not post mode"
 
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port='5000', debug=True) # 이 부분이 안먹힘. 이유는 모름.
+app.run(host='127.0.0.1', port=5000, debug=False)
