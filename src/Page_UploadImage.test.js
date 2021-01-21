93f7ae04c7a8650134b7cf8395377f70eabc3926
Soobin Jung <76704035+SoobinJung1013@users.noreply.github.com>
2021-01-21 11:25:53 +0900

    Delete Page_UploadImage.test.js

--- src/Page_UploadImage.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import UploadImage from './Page_UploadImage';
-
-test('renders learn react link', () => {
-  render(<UploadImage />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
