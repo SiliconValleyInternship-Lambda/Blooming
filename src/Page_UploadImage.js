fb8c2272449d69314729f0e5bb7090e24d07d7e8
MHJworld <50094131+MHJworld@users.noreply.github.com>
2021-01-22 13:57:40 +0900

    Update Page_UploadImage.js

--- src/Page_UploadImage.js
+++ src/Page_UploadImage.js
@@ -1,18 +1,18 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Button } from 'react-bootstrap';
-import 'semantic-ui-css/semantic.min.css'
-import './Page_UploadImage.css';
-import DragDrop from './DragDrop'
-import axios from 'axios'; 
+import "semantic-ui-css/semantic.min.css";
+import "./Page_UploadImage.css";
+import DragDrop from "./DragDrop";
+import axios from "axios";
 import PropagateLoader from "react-spinners/PropagateLoader";
 import { css } from "@emotion/core";
 
 function Page_UploadImage() {
-  const [myImage, setMyImage] = useState(null)
-  const [styleImage, setStyleImage] = useState(null)
-  const [loadingState, setLoadingState] = useState(false)
-  const [resultState, setResultState] = useState(false)
+  const [myImage, setMyImage] = useState(null);
+  const [styleImage, setStyleImage] = useState(null);
+  const [loadingState, setLoadingState] = useState(false);
+  const [resultState, setResultState] = useState(null);
 
   const override = css`
     display: block;
@@ -20,8 +20,8 @@ function Page_UploadImage() {
     border-color: #ffffff;
   `;
 
-  const postImage = async() => {
-    if (myImage==null || styleImage==null) {
+  const postImage = async () => {
+    if (myImage == null || styleImage == null) {
       alert("이미지를 입력해주세요.");
       return "";
     }
@@ -35,69 +35,78 @@ function Page_UploadImage() {
     // 이미지 전송
     let response = null;
     try {
-      response = await axios.post("/send_image", formData, {
-        header: {
-          "content-type": "multipart/form-data",
-        },
-      });
-    } catch(error) { console.log(error); }
+      response = await axios
+        .post("/send_image", formData, {
+          header: {
+            "content-type": "multipart/form-data",
+          },
+          responseType: "arraybuffer",
+        })
+        .then(response => {
+          const prefix =
+            "data:" + response.headers["content-type"] + ";base64,";
+          const file = Buffer.from(response.data, "binary").toString("base64");
+          return prefix + file;
+        });
+    } catch (error) {
+      console.log(error);
+    }
 
     // 이미지 전송 및 변환 성공
-    if(response) {
+    if (response) {
       console.log(response);
       setLoadingState(false);
-      setResultState(true);
+      setResultState(response);
       setMyImage(null);
       setStyleImage(null);
       document.getElementById("submitBtn").innerHTML = "Save";
     }
-  }
+  };
 
   return (
-    <div class="App-container">
-      <div class="ui placeholder segment">
-        { !loadingState && !resultState ? (
+    <div className="App-container">
+      <div className="ui placeholder segment">
+        {!loadingState && !resultState ? (
           <div>
-            <div class="ui two column very relaxed stackable grid">
+            <div className="ui two column very relaxed stackable grid">
               {/* dropzone */}
-              <div class="middle aligned column">
-                <DragDrop setImage={setMyImage}/>
+              <div className="middle aligned column">
+                <DragDrop setImage={setMyImage} />
               </div>
-              <div class="middle aligned column">
-                <DragDrop setImage={setStyleImage}/>
+              <div className="middle aligned column">
+                <DragDrop setImage={setStyleImage} />
               </div>
             </div>
-            <div class="ui vertical divider">
-              PLUS
-            </div>
+            <div className="ui vertical divider">PLUS</div>
           </div>
-        ) : ( 
-          loadingState ? (
-            <PropagateLoader
-              css={override}
-              size={25}
-              color={"#00b5ad"}
-              loading={loadingState}
-            />
-          ) : (
-            <div><p>result</p></div>
-          )
+        ) : loadingState ? (
+          <PropagateLoader
+            css={override}
+            size={25}
+            color={"#00b5ad"}
+            loading={loadingState}
+          />
+        ) : (
+            <div className="product">
+                <img src= {resultState} alt="result" />
+            </div>
         )}
-
       </div>
-      {/* transfer 버튼 
-          - pakage.json 파일에 ["proxy": "http://127.0.0.1:5000/",] 추가함. */}
-      <div class="btn_transfer">
-        <button class="ui inverted button" onClick = {postImage} id="submitBtn"> 
-          Transfer 
+      <div className="btn_transfer">
+        <button
+          className="ui inverted button"
+          onClick={postImage}
+          id="submitBtn"
+        >
+          Transfer
         </button>
       </div>
     </div>
-  )
+  );
 }
 
 // [DEBUG]
-function checkFormData (formData) {
+function checkFormData(formData) {
   for (let key of formData.keys()) {
     console.log(key); // FormData의 key 확인
   }
