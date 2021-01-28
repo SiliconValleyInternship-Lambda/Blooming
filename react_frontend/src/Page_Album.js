71fafdb9f28060455ccf411a17dd214d2c8b8fca
MHJworld <50094131+MHJworld@users.noreply.github.com>
2021-01-28 17:20:54 +0900

    Update Page_Album.js

--- react_frontend/src/Page_Album.js
+++ react_frontend/src/Page_Album.js
@@ -1,16 +1,15 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import NavigationBar from './NavigationBar'
 import './index.css';
 import Gallery from 'react-grid-gallery';
 import axios from "axios";
-
+/*
 const IMAGES =
 [{
         src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
         thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 174,
-        isSelected: true,
         caption: "After Rain (Jeshu John - designerspics.com)"
 },
 {
@@ -26,27 +25,50 @@ const IMAGES =
         src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
         thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
         thumbnailWidth: 320,
-        thumbnailHeight: 212
+        thumbnailHeight: 212,
+        tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+        caption: "Boats (Jeshu John - designerspics.com)"
 }]
+*/
 
-function Page_Album({history}) {
+function Page_UploadImage({history}) {
+    const [imgs, setImgs] = useState([{
+        src: "",
+        thumbnail: "",
+        thumbnailWidth: "",
+        thumbnailHeight: "",
+        tags: [{value: "", title: ""}],
+        caption: ""
+    }]);
+    
     useEffect(() => {  
         axios.get("/get_album")
         .then((Response) => {
-            console.log(Response.data);
-
+            const imgs=[]
+            for (var i = 0; i < Response.data.length; i++) {
+                imgs.push(
+                    {
+                    src : Response.data[i].src,
+                    thumbnail : Response.data[i].src,
+                    thumbnailWidth : 350,
+                    thumbnailHeight : 210,
+                    tags : [{value:Response.data[i].name, title:Response.data[i].name}],
+                    caption: Response.data[i].author + " / "+ Response.data[i].name
+                }
+            )}
+            setImgs(imgs)
         }).catch((Error) => {
             console.log(Error);
         })
     }, [])
-        
+
     return (
         <NavigationBar history={history} icon={"camera"} pageName={"ALBUM"} content={
             <div className="App-container">
-                <Gallery images={IMAGES}/>
+                <Gallery images={imgs}/>
             </div>
         }/>
     )
 }
 
-export default Page_Album;
+export default Page_UploadImage;
