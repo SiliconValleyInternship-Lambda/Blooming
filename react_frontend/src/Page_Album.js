b375c7484ead15ab0f0904d6bec4a7e67ebe3f9b
boyuuuun <44187125+boyuuuun@users.noreply.github.com>
2021-01-28 18:16:00 +0900

    add loading state to album page

--- react_frontend/src/Page_Album.js
+++ react_frontend/src/Page_Album.js
@@ -3,35 +3,18 @@ import NavigationBar from './NavigationBar'
 import './index.css';
 import Gallery from 'react-grid-gallery';
 import axios from "axios";
-/*
-const IMAGES =
-[{
-        src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-        thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-        thumbnailWidth: 320,
-        thumbnailHeight: 174,
-        caption: "After Rain (Jeshu John - designerspics.com)"
-},
-{
-        src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-        thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-        thumbnailWidth: 320,
-        thumbnailHeight: 212,
-        tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
-        caption: "Boats (Jeshu John - designerspics.com)"
-},
+import { css } from "@emotion/core";
+import DotLoader from "react-spinners/DotLoader";
 
-{
-        src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-        thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-        thumbnailWidth: 320,
-        thumbnailHeight: 212,
-        tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
-        caption: "Boats (Jeshu John - designerspics.com)"
-}]
-*/
 
-function Page_UploadImage({history}) {
+function Page_Album({history}) {
+    const override = css`
+        display: block;
+        margin: 0 auto;
+        border-color: #ffffff;
+    `;
+
+    const [loadingState, setLoadingState] = useState(true);
     const [imgs, setImgs] = useState([{
         src: "",
         thumbnail: "",
@@ -40,35 +23,47 @@ function Page_UploadImage({history}) {
         tags: [{value: "", title: ""}],
         caption: ""
     }]);
-    
+
     useEffect(() => {  
         axios.get("/get_album")
         .then((Response) => {
-            const imgs=[]
+            console.log("response - ok"); //[DEBUG]
+            const imgs=[];
             for (var i = 0; i < Response.data.length; i++) {
-                imgs.push(
-                    {
+                imgs.push({
                     src : Response.data[i].src,
                     thumbnail : Response.data[i].src,
-                    thumbnailWidth : 350,
-                    thumbnailHeight : 210,
+                    // thumbnailWidth : 350,
+                    // thumbnailHeight : 210,
                     tags : [{value:Response.data[i].name, title:Response.data[i].name}],
                     caption: Response.data[i].author + " / "+ Response.data[i].name
-                }
-            )}
-            setImgs(imgs)
+                })
+            }
+            setImgs(imgs);
+            setLoadingState(false);
         }).catch((Error) => {
             console.log(Error);
         })
     }, [])
-
+        
     return (
         <NavigationBar history={history} icon={"camera"} pageName={"ALBUM"} content={
-            <div className="App-container">
-                <Gallery images={imgs}/>
-            </div>
+            loadingState ? 
+            (
+                // loading
+                <DotLoader
+                css={override}
+                size={25}
+                color={"#00b5ad"}
+                loading={loadingState}
+                />
+            ) : (
+                <div className="App-container">
+                    <Gallery images={imgs}/>
+                </div>
+            )
         }/>
     )
 }
 
-export default Page_UploadImage;
+export default Page_Album;
